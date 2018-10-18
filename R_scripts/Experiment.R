527a28e2db7e06d73f1df0559f7cb804b47148fb
Miguel <coquert@gmail.com>
2018-10-18 14:35:46 +0200

    Added average and performance category to the dataframe

--- R_scripts/Experiment.R
+++ R_scripts/Experiment.R
@@ -1,5 +1,5 @@
 # To get a dataframe with all the data of the webapges and their energy consumed on each run,
-# Call the function get_all_data(dir_path) where dir_path is the path to the directory with the name of the device in the output files.
+# Call the function get_energy_data(dir_path) where dir_path is the path to the directory with the name of the device in the output files.
 # There is an example at the end of the file
 
 #Return the row where the data for time is empty
@@ -61,7 +61,7 @@ get_data_from_dir<-function(webpage,dir_path){
 }
 
 #Given the path of the folder that contains the directories of all the webpages of the experiment it returns a data frame with the information of all the experiments
-get_all_data<-function(dir_path){
+get_energy_data<-function(dir_path){
   dirs <- list.dirs(path=dir_path,recursive = FALSE)
   #x <- data.frame("Webpage"=character(),"Energy_consumption"=double(),stringsAsFactors = FALSE)
   x <- data.frame("Webpage"=character(),"Energy_consumption"=double(),stringsAsFactors = TRUE)
@@ -73,13 +73,67 @@ get_all_data<-function(dir_path){
   return(x)
 }
 
+#Given a dataframe with the webpages and their energy consumption on each iteration, returns a new data frame with the webpages and the mean of their energy consumption
+get_mean_data<-function(data){
+  mean_data <- data.frame("Webpage"=character(),"Mean_energy_consumption"=double(),stringsAsFactors = TRUE)
+  #mean_data <- data.frame("Webpage"=character(),"Energy_consumption"=double(),stringsAsFactors = FALSE)
+  for (webpage in levels(data$Webpage)) {
+    webpage_data <- data[which(data$Webpage == webpage),]
+    webpage_mean <- mean(webpage_data$Energy_consumption)
+    mean_data <- rbind(mean_data,data.frame("Webpage"=webpage,"Mean_energy_consupmtion"=webpage_mean))
+  }
+  return(mean_data)
+}
+
+#Creates a dataframe with the webpages and the performance caterogry based on its energy consumption. 
+#The categories are obtained with python script get_categories.py and they are added manually.
+get_performance_data<-function(){
+  performance_data <- data.frame("Webpage"=character(),"Performance"=character(),stringsAsFactors = TRUE)
+  #performance_data <- data.frame("Webpage"=character(),"Performance"=character(),stringsAsFactors = FALSE)
+  
+  performance_data <- rbind(performance_data,data.frame("Webpage"="yandexru","Performance"="Good"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="xnxxcom","Performance"="Good"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="popadsnet","Performance"="Good"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="microsoftcom","Performance"="Good"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="xvideoscom","Performance"="Good"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="youtubecom","Performance"="Good"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="askcom","Performance"="Good"))
+  
+  performance_data <- rbind(performance_data,data.frame("Webpage"="paypalcom","Performance"="Average"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="instagramcom","Performance"="Average"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="tianyacn","Performance"="Average"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="twittercom","Performance"="Average"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="applecom","Performance"="Average"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="quoracom","Performance"="Average"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="whatsappcom","Performance"="Average"))
+  
+  performance_data <- rbind(performance_data,data.frame("Webpage"="coccoccom","Performance"="Poor"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="theguardiancom","Performance"="Poor"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="ettodaynet","Performance"="Poor"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="hao123com","Performance"="Poor"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="cnncom","Performance"="Poor"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="amazonawscom","Performance"="Poor"))
+  performance_data <- rbind(performance_data,data.frame("Webpage"="chinacom","Performance"="Poor"))
+  
+  return(performance_data)
+}
+
+get_merged_data<-function(data){
+  mean_data <- get_mean_data(data)
+  performance_data <- get_performance_data()
+  merged_data <- merge(mean_data,performance_data,by = "Webpage")
+  
+  return(merged_data)
+}
 ##############################################
-data<-get_all_data("/home/mglmx/Documents/GL/repo/GreenLab-Over9000/R_scripts/Data/nx9")
+data<-get_energy_data("/home/mglmx/Documents/GL/repo/GreenLab-Over9000/R_scripts/Data/nx9")
 data
 
+View(data)
 shapiro.test(data[which(data$Webpage == "amazonawscom"),]$Energy_consumption)[[2]]
 
 par(mfrow=c(3,4));
+#Generate a QQplot and histogram of each webpage with its energy consumption
 for(webpage in levels(data$Webpage)){
   webpage_data <- data[which(data$Webpage == webpage),]
   qqnorm(webpage_data$Energy_consumption, main = webpage, ylab = "Energy Consumption")
@@ -87,8 +141,12 @@ for(webpage in levels(data$Webpage)){
   hist(webpage_data$Energy_consumption, main = webpage, xlab = "Energy Consumption")
   
   p_value <- shapiro.test(webpage_data$Energy_consumption)[[2]]
-  
   cat(webpage,p_value)
   print("")
 }
 
+#mean_data <- get_mean_data(data)
+#performance_data <- get_performance_data()
+
+merged_data <- get_merged_data(data)
+merged_data
